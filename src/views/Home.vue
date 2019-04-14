c0a5a338cfe25ef9d53d9314af7fb0a72c948bd9
Peter Jang <peter.x.jang@gmail.com>
2019-04-14 11:19:00 -0500

    Add multi-select component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <multiselect v-model="value" v-bind:options="options" v-bind:multiple="true"></multiselect>
+    </div>
+
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import Multiselect from "vue-multiselect";
+
+// register globally
+// Vue.component("multiselect", Multiselect);
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Multiselect
+  },
+  data() {
+    return {
+      value: [],
+      options: ["list", "of", "options"]
+    };
   }
-}
+};
 </script>
