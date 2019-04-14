21df9d10d500e5a36b9aab446a0986e715afe65a
Peter Jang <peter.x.jang@gmail.com>
2019-04-14 11:31:38 -0500

    Add autocomplete component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,32 @@
 <template>
   <div class="home">
     <div>
-      <multiselect v-model="value" v-bind:options="options" v-bind:multiple="true"></multiselect>
+      <multiselect v-model="value2" v-bind:options="options" v-bind:multiple="true"></multiselect>
     </div>
+
+    <label>{{ selectedEvent }}</label>
+    <vue-instant
+      :suggestOnAllWords="true"
+      :suggestion-attribute="suggestionAttribute"
+      v-model="value"
+      :disabled="false"
+      @input="changed"
+      @click-input="clickInput"
+      @click-button="clickButton"
+      @selected="selected"
+      @enter="enter"
+      @key-up="keyUp"
+      @key-down="keyDown"
+      @key-right="keyRight"
+      @clear="clear"
+      @escape="escape"
+      :show-autocomplete="true"
+      :autofocus="false"
+      :suggestions="suggestions"
+      name="customName"
+      placeholder="custom placeholder"
+      type="google"
+    ></vue-instant>
 
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
@@ -16,6 +40,12 @@
 import HelloWorld from "@/components/HelloWorld.vue";
 import Multiselect from "vue-multiselect";
 
+import Vue from "vue";
+import "vue-instant/dist/vue-instant.css";
+import VueInstant from "vue-instant";
+import axios from "axios";
+Vue.use(VueInstant);
+
 // register globally
 // Vue.component("multiselect", Multiselect);
 
@@ -27,9 +57,54 @@ export default {
   },
   data() {
     return {
-      value: [],
-      options: ["list", "of", "options"]
+      value2: [],
+      options: ["list", "of", "options"],
+
+      value: "",
+      suggestionAttribute: "original_title",
+      suggestions: [],
+      selectedEvent: ""
     };
+  },
+  methods: {
+    clickInput() {
+      this.selectedEvent = "click input";
+    },
+    clickButton() {
+      this.selectedEvent = "click button";
+    },
+    selected() {
+      this.selectedEvent = "selection changed";
+    },
+    enter() {
+      this.selectedEvent = "enter";
+    },
+    keyUp: function() {
+      this.selectedEvent = "keyup pressed";
+    },
+    keyDown: function() {
+      this.selectedEvent = "keyDown pressed";
+    },
+    keyRight: function() {
+      this.selectedEvent = "keyRight pressed";
+    },
+    clear: function() {
+      this.selectedEvent = "clear input";
+    },
+    escape: function() {
+      this.selectedEvent = "escape";
+    },
+    changed: function() {
+      var that = this;
+      this.suggestions = [];
+      axios
+        .get("https://api.themoviedb.org/3/search/movie?api_key=342d3061b70d2747a1e159ae9a7e9a36&query=" + this.value)
+        .then(function(response) {
+          response.data.results.forEach(function(a) {
+            that.suggestions.push(a);
+          });
+        });
+    }
   }
 };
 </script>
